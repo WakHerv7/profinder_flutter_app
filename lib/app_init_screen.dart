a4ef0de15079ed4d85f10371a223d3e65b1e1a52
olice333 <olice.sona@neero.io>
2024-09-01 14:38:18 +0100

    last

--- lib/app_init_screen.dart
+++ lib/app_init_screen.dart
@@ -8,7 +8,7 @@ class AppInitScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: CupertinoActivityIndicator(),
       ),
